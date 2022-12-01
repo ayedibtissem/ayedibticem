8b3eb77e86ddda3739aaba845072348cdc724fa7
ayedibtissem <ayed_ibtissem@yahoo.com>
2022-12-01 18:38:30 +0100

    first commit

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-function WordCount(str) { 
- 
-   // n = str.length;
-//return n;
- //return str.split(' ').length;
- 
-vowel=["a","e","o","i","u"];
-  count=0;
-  
-      for(i=0;i<str.length;i++){
-        if  (vowel.indexOf(str[i]) !== -1){
-          
-        count++;
-        return count;
-        }
-  }
- 
-    
-}console.log(WordCount("hello  world."));
+var set1 =[12, 13, 6, 10];
+var set2 = [13, 10, 16, 15]
 
-    
-    
-    
-   
+var sum_distinct = 0;
+var sum_commun = 0;
+var set = set1.concat(set2)
+var found = false;
+
+for(let i =0; i<set.length;i++){
+    found = false;
+    for (let j=0;j<set.length;j++){
+        if(set[i] == set[j] && i!=j){
+            found=true;
+            sum_commun += set[i];
+        }
+    }
+    if(found==false){
+        sum_distinct += set[i]
+    }
+}
+
+console.log("sum distinct el", sum_distinct )
+console.log("sum overlapping el", sum_commun ) 
